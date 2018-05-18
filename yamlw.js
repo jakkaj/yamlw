b4b19fa4b87321be5bd5cd706a9d1c43404cc2c4
Jordan Knight <jakkaj@gmail.com>
2018-05-18 13:52:55 +1000

    Fixes

--- yamlw.js
+++ yamlw.js
@@ -2,39 +2,39 @@
 // This code is licensed under MIT license (see LICENSE.txt for details)
 
 var fs = require('fs');
-var fs = require("fs-extra");
+var fs2 = require("fs-extra");
 var path = require('path');
 var yaml = require('js-yaml');
 
 
-var yamlwriter = function(file, dryrun, optionsstring){
+var yamlwriter = function (file, dryrun, optionsstring) {
     try {
 
         var doc;
-    
-        if(fs.existsSync(file)){
-            doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));    
-        }else{
+
+        if (fs.existsSync(file)) {
+            doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+        } else {
             doc = {};
         }
-    
-                var lIn = optionsstring;
+
+        var lIn = optionsstring;
         var splitted = lIn.split(',');
-    
+
         for (let item of splitted) {
 
             var splitItem = item.split('.');
             var base = doc;
-    
+
             for (let pathItem of splitItem) {
                 pathItem = pathItem.trim();
                 if (pathItem == splitItem[splitItem.length - 1]) {
                     break;
                 }
-    
+
                 let item = base[pathItem];
                 if (!item) {
-    
+
                     base[pathItem] = {};
                 }
                 base = base[pathItem];
@@ -42,12 +42,15 @@ var yamlwriter = function(file, dryrun, optionsstring){
             var lProc = "doc." + item;
             eval(lProc);
         }
-        
+
         var d = yaml.safeDump(doc);
-        
-        if(!dryrun){            
-            fs.ensureDirSync(path.dirname(file));
+
+        if (dryrun === false) {
+            console.log(`Writing file: ${file}`)
+            fs2.ensureDirSync(path.dirname(file));
             fs.writeFileSync(file, d);
+        } else {
+            console.log(`Dry-run. Skipping file write`);
         }
         //console.log(d);
 
@@ -56,6 +59,6 @@ var yamlwriter = function(file, dryrun, optionsstring){
         console.log(e);
     }
 }
-    
+
 module.exports = yamlwriter;
 
